50cd964ab5c4247aa511b5cb2289f88bc1d73b24
Lucas Allan Amorim <lucas.allan@gmail.com>
2012-05-29 11:45:11 -0300

    showing json error

--- lib/salesforceapi-rest.rb
+++ lib/salesforceapi-rest.rb
@@ -97,17 +97,17 @@ module Salesforceapi
         data = ActiveSupport::JSON::encode(parameters)
         resp = SalesforceApi::Request.do_request("POST", target, {"content-Type" => 'application/json'}, parameters)
         if (resp.code != 200) || !resp.success?
-          message = ActiveSupport::JSON.decode(resp.body)[0]["message"]
-          SalesforceApi::Errors::ErrorManager.raise_error("Authentication problem!", 401)
+          SalesforceApi::Errors::ErrorManager.raise_error(resp.body, 401)
         else
           response = ActiveSupport::JSON.decode(resp.body)
+          @instance_uri = response['instance_url']
+          @access_token = response['access_token']
+          @auth_header = {
+            "Authorization" => "OAuth " + @access_token,
+            "content-Type" => 'application/json'
+          }
         end
-        @instance_uri = response['instance_url']
-        @access_token = response['access_token']
-        @auth_header = {
-          "Authorization" => "OAuth " + @access_token,
-          "content-Type" => 'application/json'
-        }
+
       end
 
 
