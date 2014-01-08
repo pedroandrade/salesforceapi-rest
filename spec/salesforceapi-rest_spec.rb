0ddcc661170c91d259bebe58c4f0ecf95ea27b68
Pedro Andrade <pedro.rjandrade@gmail.com>
2014-01-07 22:32:33 -0300

    bug fix

--- spec/salesforceapi-rest_spec.rb
+++ spec/salesforceapi-rest_spec.rb
@@ -2,12 +2,11 @@ require 'spec_helper'
 
 describe Salesforceapi::Rest::Client do
 
-  let(:client) { SalesforceApi::Rest::Client.new("my_token", "salesforce.com") }
+  let(:client) { Salesforceapi::Rest::Client.new("1234123", "12341512", "1234123", "1234125") }
 
   it "should get the resources information" do
-    SalesforceApi::Request.should_receive(:do_request).with("GET", "salesforce.com/services/data/v21.0",
-      headers, nil).and_return(mock_get)
-    @client.resources
+    SalesforceApi::Request.should_receive(:do_request).with("POST", "https://login.salesforce.com/services/oauth2/token?grant_type=refresh_token&client_id=00DG0000000jW2VMAU&client_secret=salesforce.com&refresh_token=1234123", {"content-Type"=>"application/json"}, nil).and_return(mock_get)
+    client.resources
   end
 
 
